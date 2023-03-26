6258c2b78003edead267f62ed89b63bdd11aac7a
Michał Jasiński <michaqu11@wp.pl>
2023-03-26 03:15:03 +0200

    added login and public_key endpoints and integrating Firebase with Django

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views 
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.login, name="login"),
+    path("public_key", views.generate_public_key, name="public_key")
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,53 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,  JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# import socket
-# import threading
+import pyrebase
+import bcrypt
+import rsa
 
+firebaseConfig = {
+  "apiKey": "AIzaSyBp5y8j9lrwqZf1ptuU-35VFM1GIpLhPyw",
+  "authDomain": "fir-chat-c3185.firebaseapp.com",
+  "databaseURL": "https://fir-chat-c3185-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "fir-chat-c3185",
+  "storageBucket": "fir-chat-c3185.appspot.com",
+  "messagingSenderId": "822517477520",
+  "appId": "1:822517477520:web:b79b724e4372958057e815"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+authe = firebase.auth()
+database = firebase.database()
 
 # Create your views here.
 
 def index(response):
     return HttpResponse("<h1>S-CHAT</h1>")
+
+
+@csrf_exempt
+def login(request):
+  data = database.child('Data').child('users').get().val()
+  if request.POST.get('login') not in data:
+    salt = bcrypt.gensalt()
+    database.child('Data').child('users').push({
+      "date": request.POST.get('date'),
+      "login": request.POST.get('login'),
+      "password": bcrypt.hashpw(request.POST.get('password').encode('utf-8'), salt).decode('utf-8'),
+    })
+  
+  result = {
+    "error": False
+  }
+
+  return JsonResponse(result, safe=False)
+
+@csrf_exempt
+def generate_public_key(request):
+  public_key, private_key = rsa.newkeys(1024)
+
+  result = {
+    "public_key": '{0}'.format(public_key)
+  }
+  return JsonResponse(result, safe=False)
