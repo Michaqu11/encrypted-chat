3af1f46772968b299adf40e0fce71bb87f68a57c
Michał Jasiński <michaqu11@wp.pl>
2023-05-10 22:16:17 +0200

    added dockefile

--- schatService/settings.py
+++ schatService/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-71(7se9l=di#3d7=&0cud^mfn_2=!i+(xk4o)^pc@t5z$hq)=@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
